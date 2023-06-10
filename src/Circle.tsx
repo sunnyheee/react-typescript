ba2ea827ae9d15ab1a725bb24c0c1809594b62e2
sunnyheee <fool0116@naver.com>
2023-06-10 13:31:37 +0900

    typescript interface props

---
+++ src/Circle.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+interface ContainerProps {
+  bgColor: string;
+}
+const Container = styled.div<ContainerProps>`
+  height: 200px;
+  width: 200px;
+  background-color: ${(props) => props.bgColor};
+`;
+
+interface CircleProps {
+  bgColor: string;
+}
+
+function Circle({ bgColor }: CircleProps) {
+  return <Container bgColor={bgColor} />;
+}
+export default Circle;
